0336ec15a1491b30721009747ad4d016b71e7445
heslyja <hesly.janeth.acosta.chavez@uabc.edu.mx>
2024-03-11 19:33:36 -0700

    Agregamos la practica17, ya comenzamos a utilizar javascript

---
+++ practica17/js/practica17.js
@@ -0,0 +1,14 @@
+// Ingresamos datos staticos
+// var numero1 = 1470;
+// var numero2 = 257;
+
+// Pedimo al usuario que ingrese los numero
+var numero1= parseInt(prompt("Ingrese primer número"));
+var numero2= parseInt(prompt("Ingrese segundo número"));
+
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var division = numero1 / numero2;
+
+alert("La suma es: "+suma+"\nLa resta es: "+resta+" \nMultiplicacion: "+multiplicacion+" \nDivision: "+division+" \nGracias");
