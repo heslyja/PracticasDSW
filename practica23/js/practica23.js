7b59cccc48ab5e2a682df6aadf32e4c80795f553
heslyja <hesly.janeth.acosta.chavez@uabc.edu.mx>
2024-04-01 19:03:30 -0700

    Agregamos practica 23

---
+++ practica23/js/practica23.js
@@ -0,0 +1,10 @@
+function verificarNumero() {
+  
+    var numero = prompt("Ingresa un número:");
+
+    if (numero % 2 === 0) {
+      alert("El número " + numero + " es par");
+    } else {
+      alert("El número " + numero + " es impar");
+    }
+  }
