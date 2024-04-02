157d5a1353bc5316cb0f3ec5dae91b511013d65f
heslyja <hesly.janeth.acosta.chavez@uabc.edu.mx>
2024-04-01 19:05:01 -0700

    Agregamos practica 24

---
+++ practica24/js/practica24.js
@@ -0,0 +1,10 @@
+function avisoDeHuracan(hayUnHuracan) {
+
+    var hayUnHuracan = false; //Cambiamos a true o false dependiendo de la respuesta que queramos 
+
+  if (hayUnHuracan) {
+    alert("Hay un huracán");
+  } else {
+    alert("Está soleado");
+  }
+  }
